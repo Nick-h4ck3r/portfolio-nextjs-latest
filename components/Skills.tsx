c1cfaff2ccb89882dbf52c21363c45e46a1d372d
Nikhil Kadam <nickkadam23@gmail.com>
2023-02-13 03:25:34 +0530

    added skills icons

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,21 +21,29 @@ function Skills({}: Props) {
       </h3> */}
 
       <div className="pt-10 grid grid-cols-4 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill imgsrc="/skillsLogos/html.png" directionLeft />
+        <Skill imgsrc="/skillsLogos/css.png" directionLeft />
+        <Skill imgsrc="/skillsLogos/js.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/bootstrap.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/c.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/docker.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/express.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/framer.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/git.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/github.png" directionLeft/>
+        <Skill imgsrc="/skillsLogos/java.png" />
+        <Skill imgsrc="/skillsLogos/kuber.png" />
+        <Skill imgsrc="/skillsLogos/linux.png" />
+        <Skill imgsrc="/skillsLogos/mongodb.png" />
+        <Skill imgsrc="/skillsLogos/nextjs.png" />
+        <Skill imgsrc="/skillsLogos/node.png" />
+        <Skill imgsrc="/skillsLogos/postman.png" />
+        <Skill imgsrc="/skillsLogos/python.png" />
+        <Skill imgsrc="/skillsLogos/react.png" />
+        <Skill imgsrc="/skillsLogos/sanityio.png" />
+        <Skill imgsrc="/skillsLogos/tailwind.png" />
+        <Skill imgsrc="/skillsLogos/typescript.png" />
+        
       </div>
     </motion.div>
   );
