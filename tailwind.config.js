56d9f9f763fdd5e701b51274ca7d4e20e1fe5563
Nikhil Kadam <nickkadam23@gmail.com>
2023-08-15 18:17:11 +0530

    project add backend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,28 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'outfit': ['Outfit', 'sans-serif'],
-        'inter': ['Inter', 'sans-serif'],
+        outfit: ["Outfit", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
 
+      animation: {
+        "pulse-right-to-left":
+          "pulse-right-to-left 3s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        "pulse-right-to-left": {
+          "0%, 100%": { opacity: "0.2" },
+          "50%": { opacity: "1" },
+        },
+        "wave-fade": {
+          "0%": { opacity: "0" },
+          "20%": { opacity: "0.2" },
+          "40%": { opacity: "0.4" },
+          "60%": { opacity: "0.6" },
+          "80%": { opacity: "0.8" },
+          "100%": { opacity: "1" },
+        },
+      },
     },
   },
   plugins: [require("tailwind-scrollbar")],
