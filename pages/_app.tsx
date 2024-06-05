65b6574507b43f2cb48d98a371a8afacbfbed07d
Nikhil Kadam <nickkadam23@gmail.com>
2024-06-05 13:23:40 +0530

    remove speed insight vercel packag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   // return <Component {...pageProps} />
@@ -9,7 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
